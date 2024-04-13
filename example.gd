a058c8684b2249ab4078706a4c22ff6a77a6731f
Cyril K.B. <53737317+Cykyrios@users.noreply.github.com>
2024-04-13 15:11:25 +0200

    Refactor example

--- example.gd
+++ example.gd
@@ -1,36 +1,49 @@
-extends VBoxContainer
+extends MarginContainer
+
+
+var color_maps: Array = []
+
+@onready var vbox: VBoxContainer = %VBoxContainer
 
 
 func _ready() -> void:
-	var create_label := func create_label(text: String) -> Label:
-		var label := Label.new()
-		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-		label.text = text
-		return label
-	add_child(create_label.call("\nViridis"))
-	add_child(create_label.call("Turbo"))
-	add_child(create_label.call("Jet"))
-	add_child(create_label.call("Monochrome"))
+	add_color_map("Viridis", ColorMapViridis.new())
+	add_color_map("Turbo", ColorMapTurbo.new())
+	add_color_map("Jet", ColorMapJet.new())
+	add_color_map("Monochrome", ColorMapMonochrome.new())
 	queue_redraw()
 
 
 func _draw() -> void:
 	draw_rect(Rect2(0, 0, 1920, 1080), Color(0.5, 0.5, 0.5), true)
-	draw_color_map(ColorMapViridis.new(), 0)
-	draw_color_map(ColorMapTurbo.new(), 1)
-	draw_color_map(ColorMapJet.new(), 2)
-	draw_color_map(ColorMapMonochrome.new(), 3)
+	for i in color_maps.size():
+		draw_color_map(color_maps[i][1] as ColorMap, i)
+
+
+func add_color_map(text: String, color_map: ColorMap) -> void:
+	var label := Label.new()
+	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	label.text = text
+	vbox.add_child(label)
+	color_maps.append([text, color_map])
 
 
 func draw_color_map(color_map: ColorMap, offset: int) -> void:
-	var divisions: Array[int] = [256, 6]
+	var offset_h := 140
+	var offset_v := 32
+	var length := 300
+	var height := 50
+	var thickness := 20
+	var divisions: Array[int] = [256, 11]
 	for i in divisions.size():
 		var div := divisions[i]
 		var points := PackedVector2Array()
 		var colors := PackedColorArray()
 		for j in div:
-			var _discard := points.append(Vector2(120 + j * 256.0 / div + 300 * i, 40 + offset * 50))
-			_discard = points.append(Vector2(120 + (j + 1) * 256.0 / div + 300 * i, 40 + offset * 50))
+			var _discard := points.append(Vector2(offset_h + j * 256.0 / div + length * i,
+					offset_v + offset * height))
+			_discard = points.append(Vector2(offset_h + (j + 1) * 256.0 / div + length * i,
+					offset_v + offset * height))
 			var step := 256.0 / div
 			_discard = colors.append(color_map.get_color(j * (step + step / maxf(div - 1, 1)) / 256))
-		draw_multiline_colors(points, colors, 10)
+		draw_multiline_colors(points, colors, thickness)
